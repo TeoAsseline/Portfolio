75dff536d29527e35dbb1e981e5050c49f8f0bf0
Téo Asseline <asselineteo@gmail.com>
2024-02-06 14:21:59 +0100

    host portfolio

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// Récupérer le paramètre de la page à afficher
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+include './Page/Menu.html';
+// Vérifier le paramètre et inclure la page correspondante
+switch ($page) {
+    case 'Contact':
+        include './Page/Contact.html';
+        break;
+    case 'Parcours':
+        include './Page/Parcours.html';
+        break;
+    case 'Competences':
+        include './Page/Competence.html';
+        break;
+    case 'Projets':
+        include './Page/Projet.html';
+        break;
+    default:
+        include './Page/Presentation.html';
+        break;
+}
+include './Page/Footer.html';
+?>
+<!-- projet sous card et clic = tourne pour autre donnée -->
